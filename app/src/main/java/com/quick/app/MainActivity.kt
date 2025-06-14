23cad5c7dbe2193688586a9f042814a63de9c951
root <yunomega@email.com>
2025-06-14 17:06:55 +0800

    project init

--- app/src/main/java/com/quick/app/MainActivity.kt
+++ app/src/main/java/com/quick/app/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.quick.app
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,15 +23,10 @@ import com.quick.app.ui.theme.ClientAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ClientAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Greeting("android")
+                MyButton("click me")
             }
         }
     }
@@ -38,10 +40,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun MyButton(title: String) {
+    Button(
+        onClick = {
+            Log.d("MyButton", "clicked")
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(text = title)
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MyButtonPreview() {
     ClientAppTheme {
-        Greeting("Android")
+        MyButton("click me")
     }
 }
